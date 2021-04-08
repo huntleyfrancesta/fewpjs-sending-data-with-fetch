1d18e10309307e655af3fc98c62f7ebd81d01fd0
francesta <huntleyfrancesta@gmail.com>
2021-04-08 11:40:24 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+// const nameH1 = document.querySelector("h1");
+let body = document.querySelector('body')
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        })
+        .then(response => response.json())
+        .then(json => body.textContent = json.id)
+        .catch(err => body.textContent = err)
+}
